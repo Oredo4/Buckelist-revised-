83ebb857a30daffab8013ad002c3d4606b5506a9
Fredrick Onyango <oredo3@gmail.com>
2017-07-20 08:51:57 +0300

    Added comments

--- tests/test_login.py
+++ tests/test_login.py
@@ -5,14 +5,17 @@ import unittest
 class LoginTests(unittest.TestCase):
 
 	def test_login_successful(self):
+		"""Tests whether the login was successful"""
 		response = self.login('oredo4@gmail.com', 'Harambee')
 		self.assertEqual(response.status_code, 200)
 		
 	def test_login_nonexistent_credentials(self):
+		"""Tests whether both credentials were entered"""
 		response = self.login('','')
 		self.assertEqual(response.status_code, 400)
 		
 	def test_login_wrong_credentials(self):
+		"""Tests whether the user entered correct credentials"""
 		response = self.login('oredo4@gmail.com','')
 		self.assertEqual(response.status_code, 400)
 		
